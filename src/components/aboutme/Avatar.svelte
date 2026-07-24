4a38e726510005c8811df800c461cfe646cb3f71
Marco Lanconelli <marko_lan@hotmail.it>
2026-07-24 09:45:24 +0200

    refactor: simplify Avatar component and adjust layout in MyHero and ProjectsFull

--- src/components/aboutme/Avatar.svelte
+++ src/components/aboutme/Avatar.svelte
@@ -1,10 +1,5 @@
-<script lang="ts">
-  import { Avatar } from '@skeletonlabs/skeleton-svelte';
-</script>
-
-<Avatar
-  class="w-40 h-40 md:w-60 md:h-60 border border-white rounded-lg overflow-hidden isolate flex items-center justify-center"
->
-  <Avatar.Image src="/me2.jpg" alt="marcolanconelli" class="w-full h-full object-cover" />
-  <Avatar.Fallback class="font-bold text-2xl uppercase">ml</Avatar.Fallback>
-</Avatar>
+<img
+  src="/me2.jpg"
+  alt="marcolanconelli"
+  class="w-40 h-40 md:w-60 md:h-60 border border-white rounded-lg object-cover"
+/>
